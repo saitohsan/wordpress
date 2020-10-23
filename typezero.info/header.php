dbdcb13928e803c6791d074d3ab350793af7d992
saitohsan <takefumi.saitoh@gmail.com>
2020-10-23 22:34:32 +0900

    Update header.php

--- typezero.info/header.php
+++ typezero.info/header.php
@@ -1,9 +1,11 @@
+<!doctype html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title><?php wp_title('｜', true, 'right'); ?></title>
         <meta name="robots" content="index, follow">
         <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="format-detection" content="telephone=no">
         <link rel="SHORTCUT ICON" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
         <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png" />
