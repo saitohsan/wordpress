26ca508302c956c8a7dce251315da0534f725ce4
saitohsan <takefumi.saitoh@gmail.com>
2020-10-26 21:33:57 +0900

    Update function.php

--- typezero.info/function.php
+++ typezero.info/function.php
@@ -1,14 +1,9 @@
 <?php
-    function themebs_enqueue_styles() {
-        wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/lib/bootstrap/css/bootstrap.min.css' );
-        wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
-    }
-
-    add_action( 'wp_enqueue_scripts', 'themebs_enqueue_styles');
-
-
-    function themebs_enqueue_scripts() {
-      wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/lib/bootstrap/js/bootstrap.bundle.min.js', array( 'jquery' ) );
-    }
-    add_action( 'wp_enqueue_scripts', 'themebs_enqueue_scripts');
- ?>
+// set up theme
+// markup with html5
+add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+// generate feed link
+add_theme_support('automatic-feed-links');
+// use eye-catch image
+add_theme_support('post-thumbnails');
+?>
