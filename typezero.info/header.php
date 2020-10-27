a557c7beb604c09bc7cdb6d599c5c442ef4fddd1
saitohsan <takefumi.saitoh@gmail.com>
2020-10-27 21:48:18 +0900

    Update header.php

--- typezero.info/header.php
+++ typezero.info/header.php
@@ -1,15 +1,14 @@
 <!doctype html>
-<html>
+<html lang="ja">
     <head>
-        <title><?php wp_title('｜', true, 'right'); ?></title>
-        <meta name="robots" content="index, follow">
-        <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="format-detection" content="telephone=no">
-        <link rel="SHORTCUT ICON" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
-        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/lib/bootstrap/css/bootstrap.min.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-        <meta name="thumbnail" content="<?php echo get_template_directory_uri(); ?>/metathum.jpg" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
         <?php wp_head(); ?>
     </head>
+    <body <?php body_class(); ?>>
+        <header>
+            <div class="header-inner">
+            </div>
+        </header>
