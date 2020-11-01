88b817fc879da61d3c62acdf713f505f2d129ed2
saitohsan <takefumi.saitoh@gmail.com>
2020-11-01 19:45:20 +0900

    Update function.php

--- typezero.info/function.php
+++ typezero.info/function.php
@@ -6,4 +6,9 @@ add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 
 add_theme_support('automatic-feed-links');
 // use eye-catch image
 add_theme_support('post-thumbnails');
+
+// カスタムメニュー
+register_nav_menu('header_nav', 'ヘッダーナビゲーション');
+register_nav_menu('footer_nav', 'フッターナビゲーション');
+
 ?>
