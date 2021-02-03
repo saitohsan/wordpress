af2a89ce4ff1b182166e392c546190da6aedbb2b
saitohsan <takefumi.saitoh@gmail.com>
2021-02-03 20:30:40 +0900

    update somethings

--- normal/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
-add_theme_support('automatic-feed-links');
-add_theme_support('post-exif_thumbnails');
