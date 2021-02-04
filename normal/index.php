3f20bbb1776510491a9e42b7347e347728696ee4
saitohsan <takefumi.saitoh@gmail.com>
2021-02-04 13:53:20 +0900

    Update index.php

--- normal/index.php
+++ normal/index.php
@@ -1,18 +1,12 @@
+<?php get_header(); ?>
 <?php
-get_header();
-
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post(); ?>
-
+if(have_posts()):
+    while(have_posts()): the_post(); ?>
     <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
     <?php the_content() ?>
-
     <?php endwhile;
-
-else :
+else:
     echo '<p>There are no posts!</p>';
-
 endif;
-
 get_footer();
 ?>
