60fe29f3439ff8461c7d9ec15c085277992e3670
saitohsan <takefumi.saitoh@gmail.com>
2021-02-04 12:00:23 +0900

    Update header.php

--- normal/header.php
+++ normal/header.php
@@ -2,11 +2,12 @@
 <html lang="ja">
     <head>
         <!-- Required meta tags -->
-        <meta charset="utf-8">
+        <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <title>BLOG TITLE</title>
+        <?php wp_head(); ?>
     </head>
     <body>
         <header>
